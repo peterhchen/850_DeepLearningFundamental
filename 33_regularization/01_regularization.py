7925c51f8a14cac66e87d4bd98e09c2c7df292a5
peterhchen <peter.hp.chen@gmail.com>
2020-06-06 16:09:57 -0700

    Regularization

---
+++ 33_regularization/01_regularization.py
@@ -0,0 +1,64 @@
+import keras
+import numpy as np
+from random import randint
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense
+from keras.optimizers import Adam
+from keras import regularizers
+
+model = Sequential([
+    Dense(units=16, input_shape=(1,), activation='relu'),
+    Dense(units=32, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
+    Dense(units=2, activation='sigmoid')
+])
+
+train_labels =  []
+train_samples = []
+
+for i in range(50):
+    # The 5% of younger individuals who did experience side effects
+    random_younger = randint(13,64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
+    
+    # The 5% of older individuals who did not experience side effects
+    random_older = randint(65,100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+
+for i in range(1000):
+    # The 95% of younger individuals who did not experience side effects
+    random_younger = randint(13,64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+    
+    # The 95% of older individuals who did experience side effects
+    random_older = randint(65,100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+
+train_labels = np.array(train_labels)
+train_samples = np.array(train_samples)
+print('\train_labels:')
+print(train_labels)
+print('\ntrain_samples:')
+print(train_samples)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
+
+print('\ntype(scaler):', type(scaler))
+print('\nscaled_train_samples:')
+print(scaled_train_samples)
+
+model.compile(optimizer = \
+    Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=20, shuffle=True, verbose=2)
+print('\nscaled_train_samples.shape:', scaled_train_samples.shape)
+print('\nscaled_train_samples:')
+print(scaled_train_samples)
+print('\ntrain_labels.shape:', train_labels.shape)
+print('\ntrain_labels', train_labels)
